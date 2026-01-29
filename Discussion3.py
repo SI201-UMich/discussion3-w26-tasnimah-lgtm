673cc8f50c0ff1f33691a4759229f691daa098db
Tasnimah Uddin <tasnimah@umich.edu>
2026-01-29 18:49:58 -0500

    created eq method

--- Discussion3.py
+++ Discussion3.py
@@ -2,7 +2,7 @@ import math
 
 class Rectangle():
     # Create the constructor "__init__" method
-
+          
     # YOUR CODE HERE
    def __init__(self, user_width, user_height):
         self.width = user_width
@@ -15,12 +15,22 @@ class Rectangle():
         return f"A rectangle with width {self.width} and height {self.height}" 
 
     # Create the "area_calculator" method
-     
+  
 
     # YOUR CODE HERE
 def area_caculator(self):
         return float(self.length * self.width)
     
+  # Create the "__eq__" method
+    #
+    # Returns a boolean value
+
+    # YOUR CODE HERE
+    def __eq__(self,other):
+     if isinstance(other,Rectangle):
+          return self.length == other.length and self.width == other.width
+     return False
+
 
 
 
